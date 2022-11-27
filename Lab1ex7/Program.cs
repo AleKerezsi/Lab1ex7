5ba57cfa44bcac3bfbcec5db0382f891b4ed1ab6
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 21:26:23 +0200

    Add project files.

---
+++ Lab1ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va verifica daca un numar citit de la tastatura este par sau impar */
+
+            Console.WriteLine("Introduceti un numar: ");
+
+            int numar = int.Parse(Console.ReadLine());
+
+            if (numar % 2 == 0)
+                Console.WriteLine("Numarul este par");
+            else Console.WriteLine("Numarul este impar");
+
+        }
+    }
+}
